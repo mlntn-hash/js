bbdcad4aeeefa92cf1e174e391fcfe04e1ee16ac
mlntn.hash` <m39299@ukr.net`>
2025-07-04 13:52:29 +0300

    added function

--- ler.js
+++ ler.js
@@ -2,3 +2,15 @@ function myFunction() {
     document.getElementById("tania2").innerHTML = "Now";
     
 }
+var a, b, c; // оголошення 3 змінних
+a = 2; // присвоїти значення 2 змінній а
+b = 4;  // присвоїти значення 4 змінній b
+c = a + b; // присвоїти сумму а і б змінній с
+
+document.getElementById("demo").innerHTML = 'Змінна с = '+ c;
+
+function olaf() {
+    document.getElementById("olaf1").innerHTML = "Dog";
+    document.getElementById("olaf2").innerHTML = "Name Olaf";
+    document.getElementById("olaf3").innerHTML = "Age 1"
+}
